64aa6b547085610cd745a475491ede7222e8444a
Carlos Ruiz <cruiz@kichink.com>
2018-05-13 23:24:22 -0500

    cosas

---
+++ api/controllers/UserController.js
@@ -0,0 +1,48 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `UserController.index()`
+   */
+  index: async function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UserController.show()`
+   */
+  show: async function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UserController.edit()`
+   */
+  edit: async function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: async function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+
+};
+
