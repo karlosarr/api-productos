abc92fe9b4838326c7c4ad62f0698c0e1bfd65f7
Carlos Ruiz <cruiz@kichink.com>
2018-05-19 09:59:22 -0500

    otra

--- config/env/staging.js
+++ config/env/staging.js
@@ -41,6 +41,7 @@ module.exports = Object.assign({}, PRODUCTION_CONFIG, {
 
     onlyAllowOrigins: [
       'http://localhost:1337',
+      'https://api-productos.herokuapp.com'
       // 'https://example-staging.herokuapp.com',
       // 'http://example-staging.herokuapp.com',
       // 'https://staging.example.com',
